e7053f0a6e90b96c323f20ef3db85b950425580d
Kaimin Li <boknoobli2@hotmail.com>
2015-11-23 21:14:50 +1300

    added a field in specials for FB authentication

--- NandosoAppV2/NandosoAppV2/Models/Specials.cs
+++ NandosoAppV2/NandosoAppV2/Models/Specials.cs
@@ -13,6 +13,6 @@ namespace NandosoAppV2.Models
         public double price { get; set; }
         public string description { get; set; }
         public int specialDate { get; set; }
-
+        public int isPremium { get; set; }
     }
 }
