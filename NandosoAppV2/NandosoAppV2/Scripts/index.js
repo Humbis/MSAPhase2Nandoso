71835130baf409c6cfc0fe1cb942bcd080e8785c
Kaimin Li <boknoobli2@hotmail.com>
2015-11-23 16:25:22 +1300

    FIx attempt for day checking

--- NandosoAppV2/NandosoAppV2/Scripts/index.js
+++ NandosoAppV2/NandosoAppV2/Scripts/index.js
@@ -9,9 +9,16 @@ function loadSpecials() {
 
 function getSpecialsTable(specialsList) {
     var specialsTable = document.getElementById("specialsList");
-
+    var weekday = new Array(7);
+    weekday[0] = "Monday";
+    weekday[1] = "Tuesday";
+    weekday[2] = "Wednesday";
+    weekday[3] = "Thursday";
+    weekday[4] = "Friday";
+    weekday[5] = "Saturday";
+    weekday[6] = "Sunday";
     for (i = 0; i < specialsList.length; i++) {
-        if (Date.getDay() == specialsList[i].specialDate) {
+        if (weekday[Date.getDay()] == specialsList[i].specialDate) {
 
 
             var row = document.createElement("tr"); //make a row
