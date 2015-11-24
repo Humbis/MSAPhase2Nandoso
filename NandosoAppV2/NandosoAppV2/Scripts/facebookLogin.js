da9a62583793644f4054636d4c049413e71700e7
Kaimin Li <boknoobli2@hotmail.com>
2015-11-24 15:36:49 +1300

    Login message changes and auto reload on login to show specials

--- NandosoAppV2/NandosoAppV2/Scripts/facebookLogin.js
+++ NandosoAppV2/NandosoAppV2/Scripts/facebookLogin.js
@@ -9,16 +9,16 @@ function statusChangeCallback(response) {
     if (response.status === 'connected') {
         // Logged into your app and Facebook.
         testAPI();
-
+        location.reload();
     } else if (response.status === 'not_authorized') {
         // The person is logged into Facebook, but not your app.
         document.getElementById('status').innerHTML = 'Please log ' +
-            'into this app.';
+            'into this app to see the premium specials';
     } else {
         // The person is not logged into Facebook, so we're not sure if
         // they are logged into this app or not.
         document.getElementById('status').innerHTML = 'Please log ' +
-            'into Facebook.';
+            'into Facebook to see premium specials';
     }
 }
 
