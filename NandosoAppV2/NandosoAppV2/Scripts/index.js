13868acb21a0e903c7a7b48c2a4f2833a8f2f630
Kaimin Li <boknoobli2@hotmail.com>
2015-11-24 15:01:49 +1300

    Fix attempt for FB.getLoginStatus

--- NandosoAppV2/NandosoAppV2/Scripts/index.js
+++ NandosoAppV2/NandosoAppV2/Scripts/index.js
@@ -11,25 +11,25 @@ function getSpecialsTable(specialsList) {
     var d = new Date;
     for (i = 0; i < specialsList.length; i++) {
         if (d.getDay() == specialsList[i].specialDate) {
-            FB.getLoginStatus(function (response) {
-                if (response.status == "connected" || specialsList[i].isPremium == 0) {
-                    var row = document.createElement("tr"); //make a row
+            var response = FB.getLoginStatus();
+            if (response.status == "connected" || specialsList[i].isPremium == 0) {
+                var row = document.createElement("tr"); //make a row
 
-                    var name = document.createElement("td"); //make a data entrie
-                    name.innerHTML = specialsList[i].name;   //set it to the name in the list of specials
-                    row.appendChild(name);                   //put it into the row
+                var name = document.createElement("td"); //make a data entrie
+                name.innerHTML = specialsList[i].name;   //set it to the name in the list of specials
+                row.appendChild(name);                   //put it into the row
 
-                    var price = document.createElement("td");
-                    price.innerHTML = specialsList[i].price;
-                    row.appendChild(price);
+                var price = document.createElement("td");
+                price.innerHTML = specialsList[i].price;
+                row.appendChild(price);
 
-                    var description = document.createElement("td");
-                    description.innerHTML = specialsList[i].description;
-                    row.appendChild(description);
+                var description = document.createElement("td");
+                description.innerHTML = specialsList[i].description;
+                row.appendChild(description);
 
-                    specialsTable.appendChild(row);                  //put row into the table
-                }
-            });
+                specialsTable.appendChild(row);                  //put row into the table
+            }
+            
         }
     }
 }
