9b3e2ed4896916285d3b2590cd657c80b28cb5a9
Kaimin Li <boknoobli2@hotmail.com>
2015-11-24 16:56:58 +1300

    debug messages added

--- NandosoAppV2/NandosoAppV2/Scripts/index.js
+++ NandosoAppV2/NandosoAppV2/Scripts/index.js
@@ -1,8 +1,14 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    loadSpecials();
+﻿var onLoginClick = 0;
+document.addEventListener("DOMContentLoaded", function () {
+    loadSpecials(0);
 });
 
-function loadSpecials() {
+function loadSpecials(isPrem) {
+    if (isPrem == 0) {
+        onLoginClick = 0;
+    } else {
+        onLoginClick = 1;
+    }
     SpecialsModule.getSpecials(getSpecialsTable);
 }
 
@@ -20,9 +26,25 @@ function getSpecialsTable(specialsList) {
                 }
             });
             console.log(loggedIn);
-            if (loggedIn == 1 || specialsList[i].isPremium == 0) {
+            if ((loggedIn == 1 || specialsList[i].isPremium == 0) && onLoginClick == 0) {
                 var row = document.createElement("tr"); //make a row
+                console.log("lalallalalala");
+                var name = document.createElement("td"); //make a data entrie
+                name.innerHTML = specialsList[i].name;   //set it to the name in the list of specials
+                row.appendChild(name);                   //put it into the row
 
+                var price = document.createElement("td");
+                price.innerHTML = specialsList[i].price;
+                row.appendChild(price);
+
+                var description = document.createElement("td");
+                description.innerHTML = specialsList[i].description;
+                row.appendChild(description);
+
+                specialsTable.appendChild(row);                  //put row into the table
+            } else if (onLoginClick == 1 && specialsList[i].isPremium == 1) {
+                var row = document.createElement("tr"); //make a row
+                console.log("if dis shows it should be making normal specials")
                 var name = document.createElement("td"); //make a data entrie
                 name.innerHTML = specialsList[i].name;   //set it to the name in the list of specials
                 row.appendChild(name);                   //put it into the row
